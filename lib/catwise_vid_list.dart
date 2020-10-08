8a2d2aad6a9fcd71745fe56a05437c8aa5c35849
Nidheesh krishna <nkrishna005@gmail.com>
2020-10-08 13:07:53 +0530

    latest code

--- lib/catwise_vid_list.dart
+++ lib/catwise_vid_list.dart
@@ -1,56 +1,83 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import './models/all_model.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pregnancy_tube/video_player_page.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
+import './models/all_model.dart';
 
-class Catwise_vid_list extends StatelessWidget {
-final CatData catData;
-String id;
+class Catwise_vid_list extends StatefulWidget {
+  final CatData catData;
 
-Catwise_vid_list(this.catData);
+  Catwise_vid_list(this.catData);
+
+  @override
+  _Catwise_vid_listState createState() => _Catwise_vid_listState();
+}
+
+class _Catwise_vid_listState extends State<Catwise_vid_list> {
+  String id;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      /**/
       children: [
         Padding(
           padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
-          child: Text(catData.name,
-          style: TextStyle(fontSize: 16,
-      color: Colors.white,
-      fontWeight: FontWeight.w900,
-          ),
-      
+          child: FittedBox(
+            child: Text(
+              widget.catData.name,
+              softWrap: true,
+              style:TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
           ),
         ),
-
         Container(
           color: Colors.black,
-          height: MediaQuery.of(context).size.width * 0.4,
+          height: MediaQuery.of(context).size.width * 0.25,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
-       
-   // shrinkWrap: true,
-            itemCount: catData.videos.length,
+
+            // shrinkWrap: true,
+            itemCount: widget.catData.videos.length,
             itemBuilder: (context, index) {
-              id = YoutubePlayer.convertUrlToId(catData.videos[index].url).toString();
-              return Container(
-               // color: Colors.blue,
-                //height: 100,
-                width: 150,
-               margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20.0),
-                
-                  child: Image.network('https://img.youtube.com/vi/$id/mqdefault.jpg',
-                  //fit: BoxFit.fill,
+              id = YoutubePlayer.convertUrlToId(widget.catData.videos[index].url)
+                  .toString();
+              return Center(
+                child: GestureDetector(
+                  onTap: (){
+                    var cat=widget.catData.id;
+                    var vid=index;
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => video_player(index,cat,widget.catData)),
+                    );
+                    //$id
+
+                  },
+                  child: Card(
+                    elevation: 1,
+                    clipBehavior: Clip.antiAlias,
+                    child: CachedNetworkImage(
+                      imageUrl: 'https://img.youtube.com/vi/$id/mqdefault.jpg',
+                      width: 160,
+                      height: 200,
+                      fit: BoxFit.fill,
+                      //placeholder: (context, url) => CircularProgressIndicator(),
+                      //errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
                   ),
-                  
-                  ));
+                ),
+              );
             },
           ),
         )
-
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pregnancy_tube/home.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: SafeArea(
-              child: Scaffold(
+        child: Scaffold(
           appBar: AppBar(
             elevation: 0,
             backgroundColor: Colors.black,
